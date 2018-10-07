cc5c8a320c3213954e53bbd1751d4ebd9b1eaa73
yaqing <shanyaqing@sina.com>
2018-10-07 21:52:23 +0800

    head

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
-    home
+    <home-header></home-header>
   </div>
 </template>
 
 <script>
+import HomeHeader from '@/pages/home/components/Header.vue'
 export default {
-
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
 }
 </script>
 
---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+  <header>
+    <div class="header-left">左</div>
+    <div class="header-mid">中</div>
+    <div class="header-right">右</div>
+  </header>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus">
+  header
+    display: flex
+    height: .88rem
+    align-item: center
+    background: #00bcd4
+    color: #fff
+</style>
