67bc462d660276c74580cf9bc83955c630cd86b9
yaqing <shanyaqing@sina.com>
2018-10-08 20:51:43 +0800

    header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
   <header>
-    <div class="header-left">左</div>
-    <div class="header-mid">中</div>
-    <div class="header-right">右</div>
+    <div class="header-left">
+      <span class="iconfont">&#xe624;</span>
+    </div>
+    <div class="header-mid"><span class="iconfont">&#xe632;</span>输入城市/景点/游玩主题</div>
+    <div class="header-right">南京<span class="iconfont">&#xe6aa;</span></div>
   </header>
   </div>
 </template>
@@ -18,7 +20,21 @@ export default {
   header
     display: flex
     height: .88rem
-    align-item: center
+    align-items: center
     background: #00bcd4
     color: #fff
+    span
+      padding: 0 .1rem
+    .header-left
+      width: .6rem
+    .header-mid
+      flex: 1
+      height: .6rem
+      line-height: .6rem
+      border-radius: .06rem
+      padding-left: .2rem
+      background: #fff
+      color: #e4e7ea
+    .header-right
+      padding: 0 .22rem
 </style>
