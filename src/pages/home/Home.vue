05473a765a9db1b6d3e2d48c95d3cb854f005983
yaqingshan <shanyaqing@sina.com>
2018-10-12 16:24:16 +0800

    add

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <home-header></home-header>
+    <home-swiper></home-swiper>
   </div>
 </template>
 
 <script>
 import HomeHeader from '@/pages/home/components/Header.vue'
+import HomeSwiper from '@/pages/home/components/Swiper.vue'
 export default {
   name: 'Home',
   components: {
-    HomeHeader
+    HomeHeader,
+    HomeSwiper
   }
 }
 </script>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+  swiper
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus">
+
+</style>
