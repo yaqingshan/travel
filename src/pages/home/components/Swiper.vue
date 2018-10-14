8e7b819ad31d16d652355466bcc48001437cd055
yaqing <shanyaqing@sina.com>
2018-10-14 10:51:22 +0800

    change

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -1,42 +1,48 @@
 <template>
-<<<<<<< HEAD
-  <swiper :options="swiperOption">
-    <!-- slides -->
-    <swiper-slide>I'm Slide 1</swiper-slide>
-    <swiper-slide>I'm Slide 2</swiper-slide>
-    <swiper-slide>I'm Slide 3</swiper-slide>
-    <swiper-slide>I'm Slide 4</swiper-slide>
-    <swiper-slide>I'm Slide 5</swiper-slide>
-    <swiper-slide>I'm Slide 6</swiper-slide>
-    <swiper-slide>I'm Slide 7</swiper-slide>
-    <!-- Optional controls -->
-    <div class="swiper-pagination"  slot="pagination"></div>
-    <div class="swiper-button-prev" slot="button-prev"></div>
-    <div class="swiper-button-next" slot="button-next"></div>
-    <div class="swiper-scrollbar"   slot="scrollbar"></div>
-  </swiper>
-=======
-  <div>
-  swiper
+  <div class="swiper-box">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.url">
+      </swiper-slide>
+      <!-- Optional controls -->
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
   </div>
->>>>>>> 05473a765a9db1b6d3e2d48c95d3cb854f005983
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
   name: 'HomeSwiper',
   data () {
     return {
-      swiperOption: {}
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true
+      },
+      swiperList: [{
+        id: 1,
+        url: 'http://img1.qunarzz.com/piao/fusion/1809/87/534cf2e0a71fff02.jpg_750x200_5c778719.jpg'
+      },
+      {
+        id: 2,
+        url: 'http://img1.qunarzz.com/piao/fusion/1801/6f/3bf4c3ba2864d602.jpg_750x200_2881def8.jpg'
+      }]
     }
   }
-=======
-  name: 'HomeHeader'
->>>>>>> 05473a765a9db1b6d3e2d48c95d3cb854f005983
 }
 </script>
 
-<style lang="stylus">
-
+<style lang="stylus" scoped>
+  // 穿透样式
+  .swiper-box >>> .swiper-pagination-bullet-active
+    background: #fff !important
+  .swiper-box
+    width: 100%
+    overflow: hidden
+    height: 0
+    padding-bottom: 26.7%
+    background: #cfcfcf
+    .swiper-img
+      width: 100%
 </style>
