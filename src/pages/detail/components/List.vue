d8851325bcf095ae43ab09c077b88ff327db83c3
yaqingshan <shanyaqing@sina.com>
2018-11-09 16:42:36 +0800

    'data'

--- src/pages/detail/components/List.vue
+++ src/pages/detail/components/List.vue
@@ -2,7 +2,10 @@
   <div class="list">
     <ul>
       <li v-for="item of list" :key="item.id">
-          {{item.title}}
+          <div class="info border-bottom">
+            <span class="icon"></span>
+            {{item.title}}
+          </div>
           <div class="sec" v-if="item.children">
             <detail-list :list="item.children"></detail-list>
           </div>
@@ -21,4 +24,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+  .sec
+    padding: 0 .2rem
+  .list
+    li
+      font-size: .32rem
+      background: #fff
+      .info
+        padding: .2rem 0
+        display: flex
+        align-items: center
+        &:before
+          border-color: rgba(0,0,0,0.25)
+        .icon
+          display: block
+          margin: 0 .2rem
+          width: .36rem
+          height: .36rem
+          background: url(http://s.qunarzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+          background-size: .4rem 3rem
 </style>
